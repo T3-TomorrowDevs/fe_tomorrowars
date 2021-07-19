b4450ee3bb191c6f73fb14d28d255ba7e255072c
Angela Busato <busato.angela@gmail.com>
2021-07-19 11:51:54 +0200

    [FEAT] Create app routing close #10

--- src/feature/App/App.js
+++ src/feature/App/App.js
@@ -1,20 +1,19 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { ResizeObserver } from "@juggle/resize-observer";
-import Earth from '../../components/Earth/Earth';
-import TopSection from '../../components/TopSection/TopSection';
+import HomePage from '../../components/HomePage/HomePage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function App() {
     return (
         <div className="app">
-            // Title section
-            <TopSection />
-            // 3D Canvas
-            <Canvas className="canvas" resize={{ polyfill: ResizeObserver }}>
-                <Suspense fallback={null}>
-                    <Earth />
-                </Suspense>
-            </Canvas>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
