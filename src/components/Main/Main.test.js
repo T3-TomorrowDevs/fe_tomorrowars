febf539b36db948f79c00c9d5606e8a7043da485
Angela Busato <busato.angela@gmail.com>
2021-07-22 15:01:48 +0200

    [FEAT] create main component close #23

---
+++ src/components/Main/Main.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '@testing-library/jest-dom';
+
+import { mount } from 'enzyme';
+
+import Main from "./Main";
+
+describe('Main testing', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(<Main />);
+    });
+
+    it('should contain Main text', () => {
+        expect(wrapper.find('h1').text()).toContain("Main");
+    });
+});
