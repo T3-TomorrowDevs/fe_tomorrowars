4353d5b7952f21bae0cc01dfe841b5bc80b57b6f
Angela Busato <busato.angela@gmail.com>
2021-07-22 14:57:02 +0200

    [FEAT] create sidebar component close #22

---
+++ src/components/Sidebar/Sidebar.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '@testing-library/jest-dom';
+
+import { mount } from 'enzyme';
+
+import Sidebar from "./Sidebar";
+
+describe('Sidebar testing', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(<Sidebar />);
+    });
+
+    it('should contain Sidebar text', () => {
+        expect(wrapper.find('h1').text()).toContain("Sidebar");
+    });
+});
