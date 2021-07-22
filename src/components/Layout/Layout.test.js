33eade18942be40e0c5e69caecbbed18b91c2494
Angela Busato <busato.angela@gmail.com>
2021-07-22 15:59:27 +0200

    [FEAT] complete layout close #31

--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -13,6 +13,14 @@ describe('Layout testing', () => {
     });
 
     it('should contain Header component', () => {
-        expect(wrapper.find('h1').text()).toContain("Header");
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+
+    it('should contain Sidebar component', () => {
+        expect(wrapper.find('Sidebar')).toHaveLength(1);
+    });
+
+    it('should contain Main component', () => {
+        expect(wrapper.find('Main')).toHaveLength(1);
     });
 });
