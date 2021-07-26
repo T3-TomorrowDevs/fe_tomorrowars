eb322dab9bc07ab88a63baa33ae5c45dfdb54c77
Angela Busato <busato.angela@gmail.com>
2021-07-26 14:19:08 +0200

    [FEAT] create shop content close #42

---
+++ src/components/Card/Card.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function Card({ property }) {
+    const { troop_id, troop_name, troop_picture, troop_level, troop_cost, troop_att, troop_def, troop_quantity } = property;
+    const { handleSubmit, register } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(troop_id);
+        console.log(data);
+    }
+
+    return (
+        <div id={troop_id} className="card">
+            <img src={troop_picture} alt={troop_name} />
+            <div className="details">
+                {/* <span className="index">{troop_id}</span> */}
+                <p className="location">
+                    Name: {troop_name}<br />
+                    Level: {troop_level}
+                </p>
+                <ul className="features">
+                    <li><span>Cost </span>{troop_cost}</li>
+                    <li><span>Att/Def </span>{troop_att}/{troop_def}</li>
+                    {/* <li><span>Def </span>{troop_def}</li> */}
+                </ul>
+                {/* <div className="card__buy flex flex-row"> */}
+                    {/* <input className="card__input" type="text" placeholder={troop_num} name={troop_name}/>
+                    <button type="button" onClick={(e) => handleSubmit(e)}>BUY</button> */}
+
+                    <form className="card__buy flex flex-row" onSubmit={handleSubmit(onSubmit)}>
+                        <input className="card__input" {...register("troop_quantity")} id={troop_quantity} placeholder={troop_quantity}/>
+                        <button type="submit" >BUY</button>
+                    </form>
+                {/* </div> */}
+            </div>
+        </div>
+    )
+}
---
+++ src/data/data.js
@@ -0,0 +1,110 @@
+const data = {
+    "troops": [
+        {
+            "troop_id": 1,
+            "troop_name": "troop_1",
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            "troop_level": 1,
+            "troop_cost": 100,
+            "troop_att": 50,
+            "troop_def": 50,
+            "troop_quantity": 0
+        },
+        {
+            "troop_id": 2,
+            "troop_name": "troop_2",
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            "troop_level": 1,
+            "troop_cost": 110,
+            "troop_att": 60,
+            "troop_def": 40,
+            "troop_quantity": 0
+        },
+        {
+            "troop_id": 3,
+            "troop_name": "troop_3",
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            "troop_level": 1,
+            "troop_cost": 120,
+            "troop_att": 40,
+            "troop_def": 60,
+            "troop_quantity": 0
+        },
+        {
+            "troop_id": 4,
+            "troop_name": "troop_4",
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            "troop_level": 1,
+            "troop_cost": 80,
+            "troop_att": 20,
+            "troop_def": 80,
+            "troop_quantity": 0
+        },
+        {
+            "troop_id": 5,
+            "troop_name": "troop_5",
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            "troop_level": 1,
+            "troop_cost": 100,
+            "troop_att": 80,
+            "troop_def": 20,
+            "troop_quantity": 0
+        },
+        {
+            'troop_id': 6,
+            'troop_name': 'troop_6',
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            'troop_level': 3,
+            'troop_cost': 150,
+            'troop_att': 70,
+            'troop_def': 70,
+            "troop_quantity": 0
+        },
+
+        {
+            'troop_id': 7,
+            'troop_name': 'troop_7',
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            'troop_level': 4,
+            'troop_cost': 160,
+            'troop_att': 80,
+            'troop_def': 70,
+            "troop_quantity": 0
+        },
+
+        {
+            'troop_id': 8,
+            'troop_name': 'troop_8',
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            'troop_level': 4,
+            'troop_cost': 170,
+            'troop_att': 80,
+            'troop_def': 80,
+            "troop_quantity": 0
+        },
+
+        {
+            'troop_id': 9,
+            'troop_name': 'troop_9',
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            'troop_level': 5,
+            'troop_cost': 180,
+            'troop_att': 90,
+            'troop_def': 80,
+            "troop_quantity": 0
+        },
+
+        {
+            'troop_id': 10,
+            'troop_name': 'troop_10',
+            "troop_picture": "https://i1.sndcdn.com/artworks-j8xjG7zc1wmTeO7b-O6l83w-t500x500.jpg",
+            'troop_level': 5,
+            'troop_cost': 190,
+            'troop_att': 100,
+            'troop_def': 100,
+            "troop_quantity": 0
+        }
+    ]
+}
+
+export default data;
