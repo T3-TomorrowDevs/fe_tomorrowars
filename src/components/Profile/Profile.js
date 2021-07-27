033ddc2416df0033a0d0f3a5d49bfc36f5322273
Angela Busato <busato.angela@gmail.com>
2021-07-27 17:35:28 +0200

    [FEAT] create profile component close #46

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className="profile flex">
+            <div className="profile__info flex flex-col">
+                <span>Level: </span>
+                <span>Coin: </span>
+            </div>
+            <div>
+                <span>Name: </span>
+            </div>
+        </div>
+    )
+}
