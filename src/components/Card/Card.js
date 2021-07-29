d9a19bf9fefb6e4441cdb1aeb9cd3cf453fd62c9
Angela Busato <busato.angela@gmail.com>
2021-07-29 12:24:16 +0200

    [FEAT] handle money on submit close #58

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser, useMoney } from "../../features/user/userSlice";
 
 export default function Card({ property }) {
     const { troop_id, troop_name, troop_picture, troop_level, troop_cost, troop_att, troop_def, troop_quantity } = property;
-    const { handleSubmit, register } = useForm();
+    const { handleSubmit, register, setError, formState: { errors } } = useForm();
+
+    const user = useSelector(selectUser);
+
+    const dispatch = useDispatch();
 
     const onSubmit = (data) => {
         console.log(troop_id);
-        console.log(data);
+
+        // Checks if the user has enogh money
+        if (user.coin - (troop_cost * data.troop_quantity) >= 0) {
+
+            const money = user.coin - (troop_cost * data.troop_quantity);
+
+            dispatch(useMoney(money));
+        } else {
+            setError("troop_quantity", { type: "focus" , message: "not enough credit"}, { shouldFocus: true });
+        }
     }
 
     return (
         <div id={troop_id} className="card">
             <img src={troop_picture} alt={troop_name} />
             <div className="details">
-                {/* <span className="index">{troop_id}</span> */}
                 <p className="location">
                     Name: {troop_name}<br />
                     Level: {troop_level}
@@ -29,9 +43,10 @@ export default function Card({ property }) {
                     <button type="button" onClick={(e) => handleSubmit(e)}>BUY</button> */}
 
                 <form className="card__buy flex flex-row" onSubmit={handleSubmit(onSubmit)}>
-                    <input className="card__input" {...register("troop_quantity")} id={troop_quantity} placeholder={troop_quantity} />
+                    <input type="number" name="name" className="card__input" {...register("troop_quantity")} id={troop_id} placeholder={troop_quantity} />
                     <button type="submit" >BUY</button>
                 </form>
+                {errors.troop_quantity && <span>{errors.troop_quantity.message}</span> }
                 {/* </div> */}
             </div>
         </div>
