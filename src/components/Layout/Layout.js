903a62cd8b509a45622cb822ecf23f2acf71addd
Angela Busato <busato.angela@gmail.com>
2021-08-03 14:23:54 +0200

    [FEAT] add get user call close #70

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import Main from "../Main/Main";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGetUser, selectUser } from '../../features/user/userSlice';
 
 export default function Layout({ children }) {
+
+    const dispatch = useDispatch();
+    const user = useSelector(selectUser);
+    console.log(user);
+
+    // for fetching an API on mount
+    useEffect(() => {
+        // get user information
+        dispatch(fetchGetUser());
+    }, []);
+
     return (
         <div>
-            <Header />
-            <div className="main__container">
-                <Sidebar />
-                <Main>{children}</Main>
-            </div>
+            {user.length !== 0 &&
+                <div>
+                    <Header />
+                    <div className="main__container">
+                        <Sidebar />
+                        <Main>{children}</Main>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
