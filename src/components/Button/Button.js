4ee8f7fa3cb5fd19f58b49c90840674f5e3edf92
Angela Busato <busato.angela@gmail.com>
2021-08-10 15:34:10 +0200

    [FEAT] create link shop to galaxy close #84

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button(props) {
     return (
-        <button className={`btn ${props.typeClass}`} type={props.type} disabled={props.loading}>{props.text}</button>
+        <button className={`btn ${props.typeClass}`} type={props.type} disabled={props.loading} onClick={props.onClick}>{props.text}</button>
     )
 }
