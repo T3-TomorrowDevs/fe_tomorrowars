a03d88ff97d2c8124635cbce0707c7aa016a84e5
Angela Busato <busato.angela@gmail.com>
2021-08-20 10:58:37 +0200

    [FEAT] add planet name close #90

--- src/components/PlanetsSystem/Planet.js
+++ src/components/PlanetsSystem/Planet.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Ecliptic from './Ecliptic';
 import * as THREE from "three";
+import Roboto from '../../assets/fonts/Roboto.json';
 
-export default function Planet({ planet: { color, xRadius, zRadius, size } }) {
+export default function Planet({ planet: { color, xRadius, zRadius, size, name } }) {
+    const font = new THREE.FontLoader().parse(Roboto);
+    const textOptions = {
+        font,
+        size: .5,
+        height: .2
+    };
     return (
         <>
             <mesh position={[xRadius, 0, 0]}>
                 <sphereGeometry args={[size, 32, 32]} />
                 <meshStandardMaterial color={color} />
+            </mesh>
+            <mesh position={[xRadius-.5, -3, 0]}>
+                <textGeometry attach='geometry' args={[name, textOptions]} />
+                <meshStandardMaterial attach='material' color="yellow"/>
             </mesh>
             <Ecliptic xRadius={xRadius} zRadius={zRadius} />
         </>
