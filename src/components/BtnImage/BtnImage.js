d7a139ea04adbafe918000c22f8a50a7ffae32a2
Angela Busato <busato.angela@gmail.com>
2021-08-23 09:48:55 +0200

    [FEAT] add attack page to sidebar close #104

--- src/components/BtnImage/BtnImage.js
+++ src/components/BtnImage/BtnImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BtnImage(props) {
     return (
         <div className="btnImage__container flex flex-col align-center" onClick={props.handleClick}>
-            <img className="btnImage__img" src={props.imgSrc} alt="" />
+            <img className="btnImage__img" src={props.imgSrc} alt={props.text} />
             <span className="btnImage__text">{props.text}</span>
         </div>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import shop from "../../assets/images/sidebar-btn/reshot-icon-moon-rover-M7R4BJZ
 import galaxy from "../../assets/images/sidebar-btn/reshot-icon-jupiter-PN9BDC3RYT.png";
 import battles from "../../assets/images/sidebar-btn/reshot-icon-rocket-ship-YUMR2P8LFG.png";
 import myPlanets from "../../assets/images/sidebar-btn/reshot-icon-planet-earth-NUHXQMGP5D.png";
+import attack from "../../assets/images/sidebar-btn/reshot-icon-meteorite-JU785HLTSC.png";
 import { useHistory } from 'react-router-dom';
 
 export default function Navbar() {
@@ -18,10 +19,15 @@ export default function Navbar() {
         history.push("/galaxy");
     }
 
+    const goToAttack = () => {
+        history.push("/attack");
+    }
+
     return (
         <div className="nav">
-            <BtnImage text="shop" imgSrc={shop} handleClick={goToShop}/>
             <BtnImage text="galaxy" imgSrc={galaxy} handleClick={goToGalaxy}/>
+            <BtnImage text="shop" imgSrc={shop} handleClick={goToShop}/>
+            <BtnImage text="attack" imgSrc={attack} handleClick={goToAttack}/>
             <BtnImage text="battles" imgSrc={battles}/>
             <BtnImage text="my planets" imgSrc={myPlanets} />
         </div >
